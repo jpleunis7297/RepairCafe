b9595284fb52a0e585c89114c1ed820e6b4e316e
jpleunis7297 <35992495+jpleunis7297@users.noreply.github.com>
2018-02-07 11:41:37 +0100

    header en footer geinclude.

--- includes/footer.php
+++ includes/footer.php
@@ -5,3 +5,9 @@
  * Date: 07/02/2018
  * Time: 10:07
  */
+?>
+
+</body>
+<script src="//cdnjs.cloudflare.com/ajax/libs/less.js/2.7.2/less.min.js"></script>
+
+</html>
--- index.php
+++ index.php
@@ -1,21 +1,9 @@
+<?php
 
-<!DOCTYPE html>
-<html>
+include 'includes/header.php';
 
-  <head>
-    <link rel="icon" type="image/png" sizes="32x32" href="img/favicon-32x32.png">
 
-    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-    <script src="https://use.fontawesome.com/26bbb1ea68.js"></script>
-  </head>
-
-  <title>Home</title>
-  <body>
-
-  <link rel="stylesheet/less" type="text/css" href="style.less" />
+?>
 
     <div class="container">
 
@@ -47,9 +35,8 @@
       </div>
 
     </div>
+<?php
 
-  </body>
+include 'includes/footer.php';
 
-  <script src="//cdnjs.cloudflare.com/ajax/libs/less.js/2.7.2/less.min.js"></script>
-
-</html>
+?>
